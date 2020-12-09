4d7eb6562a54c6d48cfaef37936718bc99a1a645
chenyouneng@huanyi.onaliyun.com <chenyouneng@huanyi.onaliyun.com>
2020-12-09 13:35:00 +0800

    2

---
+++ 面向对象/@property使用.py
@@ -0,0 +1,19 @@
+class Persion:
+    def __init__(self,name):
+        self.__name = name
+    @property
+    def name(self):
+        return self.__name +'sb'
+ 
+    @name.setter#传一个参数
+    def nameset(self,new_name):
+        self.__name = new_name
+    @name.deleter
+    def name(self):
+        del self.__name
+a1 = Persion('jinijin')
+print(a1.name)
+a1.nameset = '大家'#修改属性值,    如果不适用@name.setter 则不能修改
+print(a1.name)
+del a1.name
+print(a1.name)
