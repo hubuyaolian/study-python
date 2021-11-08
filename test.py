aa97f8cc4f1f46211b3e2034585da81cd9995824
hubuyaolian <383505002@qq.com>
2021-11-08 22:49:50 +0800

    删除没用的

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-def adder(x):
-    def wrapper(y):  
-        return x + y  
-    return wrapper
-adder5 = adder(5)
-print(adder5(adder5(6)))
+# def adder(x):
+#     def wrapper(y):
+#         return x + y
+#     return wrapper
+# adder5 = adder(5)
+# print(adder5(adder5(6)))
+def sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+items = [1, 10, 7, 4, 5, 9]
+print(sum(items))
